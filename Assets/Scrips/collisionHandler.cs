a1bd2d7eda02d931768cfc9589beca7d284538fc
JPA7 <sggprodu@gmail.com>
2022-11-15 13:20:05 +0200

    44. Switch Statements

---
+++ Assets/Scrips/collisionHandler.cs
@@ -0,0 +1,24 @@
+
+using UnityEngine;
+
+public class collisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Friendly");
+                break;
+            case "Finish":
+                Debug.Log("Finish");
+                break;
+            case "Fuel":
+                Debug.Log("Fuel");
+                break;
+            default:
+                Debug.Log("Exploded");
+                break;
+        }
+    }
+}
