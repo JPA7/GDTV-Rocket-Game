92894e682cb75581bccde601875fcc7226d51f82
JPA7 <sggprodu@gmail.com>
2022-11-15 12:57:39 +0200

    43. Play AudioSource SFX

--- Assets/Scrips/Movement.cs
+++ Assets/Scrips/Movement.cs
@@ -7,11 +7,13 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rotationThrust = 100f;
     Rigidbody rocketrb;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rocketrb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,6 +28,14 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rocketrb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 
