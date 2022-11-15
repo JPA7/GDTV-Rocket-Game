e900e57d5baad611abb97d2ac986da08d1738935
JPA7 <sggprodu@gmail.com>
2022-11-15 17:09:17 +0200

    47. Using Invoke

--- Assets/Scrips/collisionHandler.cs
+++ Assets/Scrips/collisionHandler.cs
@@ -1,9 +1,11 @@
 
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class collisionHandler : MonoBehaviour
 {
+    [SerializeField] float lvlEndDelay = 1f;
     void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
@@ -12,12 +14,28 @@ public class collisionHandler : MonoBehaviour
                 Debug.Log("Friendly");
                 break;
             case "Finish":
-                NextLevel();
+                StartSuccessSequence();
                 break;
             default:
-                ReloadLevel();
+                StartCrashSequence();
                 break;
         }
+    }
+
+    void StartSuccessSequence()
+    {
+        // todo add SFX upon crash
+        // todo add particle effect upon crash
+        GetComponent<Movement>().enabled = false;
+        Invoke("NextLevel", lvlEndDelay);
+    }
+
+        void StartCrashSequence()
+    {
+        // todo add SFX upon crash
+        // todo add particle effect upon crash
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", lvlEndDelay);
     }
 
     void NextLevel()
@@ -25,14 +43,14 @@ public class collisionHandler : MonoBehaviour
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         int nextSceneIndex = currentSceneIndex + 1;
         if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
-            {
-                nextSceneIndex = 0;
-            }
+        {
+            nextSceneIndex = 0;
+        }
         SceneManager.LoadScene(nextSceneIndex);
     }
 
     void ReloadLevel()
-    {
+    {        
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
     }
